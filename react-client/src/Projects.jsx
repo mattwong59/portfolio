e63aa4b276918715383cb07b88651aba16566560
Matt Wong <mattwong59@gmail.com>
2018-06-27 12:43:10 -0700

    Add overlay for all projects

--- react-client/src/Projects.jsx
+++ react-client/src/Projects.jsx
@@ -15,100 +15,151 @@ class Projects extends React.Component {
                 <div className="tile is-parent">
                     <div className="my-container">
                         <div className="my-content">
-                            <a href="https://unsplash.com/photos/HkTMcmlMOUQ" target="_blank">
                             <div className="content-overlay">
-                            {/* <div className="content-details fadeIn-top"> */}
-                                <h3>Technologies Used:</h3><br/>
-                                <ul>
-                                    <li>React JS</li>
-                                    <li>Ruby on Rails</li>
-                                    <li>HTML</li>
-                                    <li>CSS</li>
-                                </ul>
-                                <a class="button is-outlined">Outlined</a>
-                                {/* </div>                             */}
+                                <div className="content-details fadeIn-top">
+                                    <h3>Technologies Used:</h3><br/>
+                                    <ul>
+                                        <li>React.js</li>
+                                        <li>Ruby on Rails</li>
+                                        <li>HTML</li>
+                                        <li>CSS</li>
+                                    </ul>
+                                    <a href="https://beertourapp.herokuapp.com/" target="_blank" rel="noopener noreferrer" class="button is-outlined is-rounded">
+                                        Visit Website &nbsp; <i className="fas fa-angle-right"></i>
+                                    </a>
+                                </div>                            
                             </div>
-                                {/* <figure className="image is-256x256"> */}
-                                    <img className="content-image" src={require("./docs/beercat-pub-pounce.png")} alt="Screenshot of beer cat tour page." />
-                                {/* </figure>     */}
-                            </a>
+                            <img className="content-image" src={require("./docs/beercat-pub-pounce.png")} alt="Screenshot of beer cat tour page." />
                             <h4 className="is-size-4">Beer Cat Tourism App</h4>
                         </div>
                     </div>
-                
-                    {/* <div className="title-hover">
-                           <h2> Beer Cat Tourism App </h2>
-                            <p className="is-size-6">Tools: React, Ruby on Rails, HTML, CSS</p>
-                        <h4>Technologies Used:</h4>
-                        <ul>
-                            <li>dasdsafasfas</li>
-                            <li>dasdsafasfas</li>
-                            <li>dasdsafasfas</li>
-                        </ul>
-                    </div> */}
-                    {/* <a href="https://github.com/mattwong59/Beer-Cat-Tourism-App" target="_blank" rel="noopener noreferrer" className="project-title is-size-4">
-                        <figure className="image is-256x256">
-                            <img src={require("./docs/beercat-pub-pounce.png")} alt="Screenshot of beer cat tour page."/>
-                            <h4>Beer Cat Tourism App</h4>
-                            <p className="is-size-6">Tools: React, Ruby on Rails, HTML, CSS</p>
-                        </figure>
-                    </a>  */}
                 </div>
                 <div className="tile is-parent">
-                    <a href="https://github.com/mattwong59/chatty-app" target="_blank" rel="noopener noreferrer" className="project-title is-size-4">
-                        <figure className="image is-256x256">
+                    <div className="my-container">
+                        <div className="my-content">
+                            <div className="content-overlay">
+                                <div className="content-details fadeIn-top">
+                                    <h3>Technologies Used:</h3><br/>
+                                    <ul>
+                                        <li>React.js</li>
+                                        <li>Node.js</li>
+                                        <li>Express</li>
+                                        <li>WebSockets</li>
+                                        <li>CSS</li>
+                                    </ul>
+                                    <a href="https://github.com/mattwong59/chatty-app" target="_blank" rel="noopener noreferrer" class="button is-outlined is-rounded">
+                                        Visit Repo &nbsp; <i className="fas fa-angle-right"></i>
+                                    </a>
+                                </div>                            
+                            </div>
                             <img src={require("./docs/chatty-two-users.png")} alt="Screenshot of chatty with 2 users sending messages."/>
-                            Chatty App
-                            <p className="is-size-6">Tools: React, WebSockets, Express, CSS</p>
-                        </figure>
-                    </a> 
+                            <h4 className="is-size-4">Chatty App</h4>
+                        </div>
+                    </div>                 
                 </div>
                 <div className="tile is-parent">
-                    <a href="https://github.com/mattwong59/decider_midterm" target="_blank" rel="noopener noreferrer" className="project-title is-size-4">
-                        <figure className="image is-256x256">
+                    <div className="my-container">
+                        <div className="my-content">
+                            <div className="content-overlay">
+                                <div className="content-details fadeIn-top">
+                                    <h3>Technologies Used:</h3><br/>
+                                    <ul>
+                                        <li>Node.js</li>
+                                        <li>Express</li>
+                                        <li>EJS</li>
+                                        <li>PostgreSQL</li>
+                                        <li>SASS</li>
+                                        <li>AJAX</li>
+                                    </ul>
+                                    <a href="https://decider-supreme.herokuapp.com/" target="_blank" rel="noopener noreferrer" class="button is-outlined is-rounded">
+                                        Visit Website &nbsp; <i className="fas fa-angle-right"></i>
+                                    </a>
+                                </div>                            
+                            </div>
                             <img src={require("./docs/decider.png")} alt="Screenshot of decider home page."/>
-                            Decider
-                            <p className="is-size-6">Tools: Node, Express, EJS, PSQL, SASS, AJAX.</p>
-                        </figure>
-                    </a> 
+                            <h4 className="is-size-4">Decider</h4>
+                        </div>
+                    </div>                                 
                 </div>
             </div>
             
             <div className="tile is-ancestor">
                 <div className="tile is-parent">
-                    <a href="https://github.com/mattwong59/jungle-rails" target="_blank" rel="noopener noreferrer" className="project-title is-size-4">
-                        <figure className="image is-256x256">    
+                    <div className="my-container">
+                        <div className="my-content">
+                            <div className="content-overlay">
+                                <div className="content-details fadeIn-top">
+                                    <h3>Technologies Used:</h3><br/>
+                                    <ul>
+                                        <li>Ruby on Rails</li>
+                                        <li>Ruby</li>
+                                        <li>CSS</li>
+                                    </ul>
+                                    <a href="https://github.com/mattwong59/jungle-rails" target="_blank" rel="noopener noreferrer" class="button is-outlined is-rounded">
+                                        Visit Repo &nbsp; <i className="fas fa-angle-right"></i>
+                                    </a>
+                                </div>                            
+                            </div>
                             <img src={require("./docs/jungle-resize.png")} alt="Screenshot of jungle's product page."/>
-                            Jungle   
-                            <p className="is-size-6">Tools: Ruby on Rails, Ruby</p>         
-                        </figure>
-                    </a>
-                </div>
-                    <div className="tile is-parent">
-                    <figure className="image is-256x256">
-                        <a href="https://github.com/mattwong59/tweeter" target="_blank" rel="noopener noreferrer" className="project-title is-size-4">
+                            <h4 className="is-size-4">Jungle(e-commerce app)</h4>
+                        </div>
+                    </div>                                 
+                </div>                      
+                <div className="tile is-parent">
+                    <div className="my-container">
+                        <div className="my-content">
+                            <div className="content-overlay">
+                                <div className="content-details fadeIn-top">
+                                    <h3>Technologies Used:</h3><br/>
+                                    <ul>
+                                        <li>HTML</li>
+                                        <li>CSS</li>
+                                        <li>Javascript</li>
+                                        <li>jQuery</li>
+                                        <li>MongoDB</li>
+                                        <li>AJAX</li>
+                                        <li>Node.js</li>
+                                        <li>Express</li>
+                                    </ul>
+                                    <a href="https://github.com/mattwong59/tweeter" target="_blank" rel="noopener noreferrer" class="button is-outlined is-rounded">
+                                        Visit Repo &nbsp; <i className="fas fa-angle-right"></i>
+                                    </a>
+                                </div>                            
+                            </div>
                             <img src={require("./docs/tweeter-restyle.png")} alt="Screenshot of tweeter home page"/>
-                            Tweeter
-                            <p className="is-size-6">Tools: HTML, CSS, JS, jQuery, AJAX, Node, Express, MongoDB</p>         
-                        </a>
-                    </figure>
-                    </div>
-                    <div className="tile is-parent">
-                    <figure className="image is-256x256">
-                        <a href="https://github.com/mattwong59/tinyApp" target="_blank" rel="noopener noreferrer" className="project-title is-size-4">
+                            <h4 className="is-size-4">Tweeter</h4>
+                        </div>
+                    </div>                                        
+                </div>
+                <div className="tile is-parent">
+                    <div className="my-container">
+                        <div className="my-content">
+                            <div className="content-overlay">
+                                <div className="content-details fadeIn-top">
+                                    <h3>Technologies Used:</h3><br/>
+                                    <ul>
+                                        <li>HTML</li>
+                                        <li>CSS</li>
+                                        <li>Node.js</li>
+                                        <li>Bulma</li>
+                                    </ul>
+                                    <a href="https://github.com/mattwong59/tinyApp" target="_blank" rel="noopener noreferrer" class="button is-outlined is-rounded">
+                                        Visit Repo &nbsp; <i className="fas fa-angle-right"></i>
+                                    </a>
+                                </div>                            
+                            </div>
                             <img src={require("./docs/Tiny-App-URLS.png")} alt="Screenshot of tiny app urls page."/>
-                            Tiny App
-                            <p className="is-size-6">Tools: Node, Express</p>         
-                        </a>
-                    </figure>
+                            <h4 className="is-size-4">Tiny App</h4>
+                        </div>
+                    </div>                                        
                 </div>
             </div>
-            <a href="https://github.com/mattwong59" className="button is-medium is-inverted" target="_blank" rel="noopener noreferrer">
-                <span className="icon">
-                <i className="fab fa-github"></i>
-                </span>
-                <span>GitHub</span>
-            </a>
+        <a href="https://github.com/mattwong59" className="button is-medium is-inverted" target="_blank" rel="noopener noreferrer">
+            <span className="icon">
+            <i className="fab fa-github"></i>
+            </span>
+            <span>GitHub</span>
+        </a>
         </div>
     </section>
     );
